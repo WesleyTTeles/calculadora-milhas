3762ea33d8f7e84539ea58eab6b871f9d2736dfd
wesley teles <wesley.teles.dev@gmail.com>
2022-10-24 16:26:03 -0300

    Version 2.0

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,131 +1,7 @@
-// >>>>>>>>>>>>>>>>>>>>> PROCESSO DE COMPRAR <<<<<<<<<<<<<<<<<<<<
-
-// Funcao observar as mudancas do input e pegar os valores de cada mudanca.
-function choiceOptionBuy() {
-    const select = document.querySelector('#opttions-buy');
-    valor = select.options[select.selectedIndex].value;
-}
-
-// Essa funcao primeiramente verifica se os inputs estao preenchidos, se nao estiver ele vai solicitar conforme o campo vazio.
-// se estive preenchido ela ira realizar o calcula com base nos valores do input e select e mostrará o resultado em tela.
-function calcBuy(){
-    if (checkValueBuy() == false){
-    } else {
-        let vlr_pay = document.querySelector('#vlr-to-pay').value;
-        let qtd_buy = document.querySelector('#qtd-to-buy').value;
-        let price = (vlr_pay * 1000) / qtd_buy;
-        if (valor == 1){
-            if (price <= 17.50) {
-                alert(`OPORTUNIDADE, milheiro saindo à ${price.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })} reais`);
-                remov();
-            } else {
-                alert(`PREJUIZO, milheiro saindo à ${price.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })} reais`);
-                remov();  
-            }
-        } else if (valor == 2) {
-            if (price <= 20) {
-                alert(`OPORTUNIDADE, milheiro saindo à ${price.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })} reais`);
-                remov();
-            } else {
-                alert(`PREJUIZO, milheiro saindo à ${price.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })} reais`);
-                remov(); 
-            }
-        } else if (valor == 3 || valor == 4) {
-            if (price <= 24) {
-                alert(`OPORTUNIDADE, milheiro saindo à ${price.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })} reais`);
-                remov();
-            } else {
-                alert(`PREJUIZO, milheiro saindo à ${price.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })} reais`);
-                remov(); 
-            }
-        } else {
-            alert('Preecnha os Campos!')
-        }
-    }
-}
-
-// funcao para checar se os campos estao vazios e se estiver ele irá pedir pra preencher
-function checkValueBuy(){
-    let vlr_pay = document.querySelector('#vlr-to-pay').value;
-    let qtd_buy = document.querySelector('#qtd-to-buy').value;
-    let msg = '';
-
-    if (vlr_pay == ''){
-        msg += 'Informe o Valor Pago \n';
-    }
-    if (qtd_buy == ''){
-        msg += 'Informe a quantidade de milha \n';
-    }
-    if (msg != ''){
-        alert(msg);
-        return false
-    }
-    return true;
-}
-
-// funcao para limpar os campos depois de mostrar o resultado em tela.
-function remov() {
-    let vlr_pay = document.querySelector('#vlr-to-pay').value = "";
-    let qtd_buy = document.querySelector('#qtd-to-buy').value = "";
-}
-
-// >>>>>>>>>>>>>>>>>>>> PROCESSO DE VENDA <<<<<<<<<<<<<<<<<<
-function choiceOptionSell() {
-    const selectSell = document.querySelector('#opttions-sell');
-    valorSell = selectSell.options[selectSell.selectedIndex].value;
-    
-}
-
-function calcSell(){
-    if (checkValueSell() == false) {
-    } else {
-        let simulation = document.querySelector('#vlr-simulations').value;
-        let sell = document.querySelector('#qtd-to-sell').value;
-        let priceSell = (simulation * 1000) / sell;
-        if (valorSell == 1){
-            if (priceSell >= 19) {
-                alert(`OPORTUNIDADE, milheiro saindo à ${priceSell.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })} reais`);
-                remov();
-            } else {
-                alert(`PREJUIZO, milheiro saindo à ${priceSell.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })} reais`);
-                remov();  
-            }
-        } else if (valorSell == 2) {
-            alert('Cálculo ainda nao definido, por favor tente mais tarde.');
-        } else if (valorSell == 3 || valorSell == 4) {
-            if (priceSell >= 27) {
-                alert(`OPORTUNIDADE, milheiro saindo à ${priceSell.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })} reais`);
-                remov();
-            } else { 
-                alert(`PREJUIZO, milheiro saindo à ${priceSell.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })} reais`);
-                remov(); 
-            }
-        } else {
-            alert('Preecnha os Campos!')
-        }
-    }
-}
-
-function checkValueSell(){
-    let simulation = document.querySelector('#vlr-simulations').value;
-    let sell = document.querySelector('#qtd-to-sell').value;
-    let msg1 = '';
-
-    if (simulation == ''){
-        msg1 += 'Informe o Valor Pago \n';
-    }
-    if (sell == ''){
-        msg1 += 'Informe a quantidade de milha \n';
-    }
-    if (msg1 != ''){
-        alert(msg1);
-        return false
-    }
-    return true;
-}
-function checkBuyPoit(){
-    let qtdPoints = document.querySelector('#qtd-points').value;
-    let porcentPoint = document.querySelector('#porcent').value;
+// Funcao pra checar se os campos estao vazios
+function checkBuyPoint(){
+    let qtdPoints = document.querySelector('#qtd-point').value;
+    let porcentPoint = document.querySelector('#vl-cupon').value;
     let msg1 = '';
 
     if (qtdPoints == ''){
@@ -140,61 +16,107 @@ function checkBuyPoit(){
     }
     return true;
 }
-
+// Funcao para Limpar os Campos apos o calculo.
 function remov() {
-    let simulation = document.querySelector('#vlr-simulations').value = '';
-    let sell = document.querySelector('#qtd-to-sell').value = '';
+    document.querySelector('#qtd-point').value = '';
+    document.querySelector('#vl-cupon').value = '';
+    document.querySelector('#vl-transfer').value = '';
 }
 
-function showBuy(){
-    document.querySelector('.sell').style.display = 'none';
-    document.querySelector('.buy').style.display = 'flex';
-    document.querySelector('.transfer').style.display = 'none';
-}
-function showSell(){
-    document.querySelector('.sell').style.display = 'flex';
-    document.querySelector('.buy').style.display = 'none';
-    document.querySelector('.transfer').style.display = 'none';
-}
-function showTransfer(){
-    document.querySelector('.sell').style.display = 'none';
-    document.querySelector('.buy').style.display = 'none';
-    document.querySelector('.transfer').style.display = 'flex';
-}
-
-
-
+// Funcao pra quando clica no checkbox ele mostre o input da transferencia
 let checkbox = document.querySelector('#check');
 let element = document.querySelector('.row-transfer');
 
 checkbox.addEventListener('change', ()=> {
-    let list = element.classList.toggle("myStyle");
-  
+    let list = element.classList.toggle("show-input-transfer");
 })
 
-function calcPoints() {
-    if (checkBuyPoit() == false) {
-    } else {
-        let qtdPoint = document.querySelector('#qtd-points').value
-        let porcent = document.querySelector('#porcent').value;
-        let porcentTransfer = document.querySelector('#porcent-transfer').value;
-    
-        let qtdPointNumb = parseFloat(qtdPoint);
-        let porcentNumb = parseFloat(porcent);
-        let porcentTransferNumb = parseFloat(porcentTransfer);
-        let priceMilhasReal = 70;
-        
-        priceMilhas = priceMilhasReal - ((priceMilhasReal * porcentNumb) / 100);
-        valueInvest = (qtdPointNumb / 1000) * priceMilhas;
-
-        transfer = ((qtdPointNumb * porcentTransferNumb) / 100) + qtdPointNumb;
-        custoMilheiro = valueInvest / (transfer/1000);
-        if(porcentTransfer == ''){
-            alert(`Seu Valor investido é ${valueInvest.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })} reais com um custo do milheiro de ${priceMilhas.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })} reais.`)
-
-        } else {
-            alert(`Seu Valor investido é ${valueInvest.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })} reais com um Custo do Milheiro de ${priceMilhas.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })} reais, Voce ficara com ${transfer} milhas com essa Tranferencia e o Custo do Milheiro depois da Tranferencia é de ${custoMilheiro.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })} reais`)
-        }
-    }
+// funcao para monitorar as mudancas do select e armazenar em uma variavel
+function choiceOptionBuy() {
+    const select = document.querySelector('#opttions-buy');
+    valor = select.options[select.selectedIndex].value;
 }
 
+function calcPoints() {
+    let qtdPoint = document.querySelector('#qtd-point').value;
+    let vlCupom = document.querySelector('#vl-cupon').value;
+    let vlTransfer = document.querySelector('#vl-transfer').value;
+    let qtdPointNumb = parseFloat(qtdPoint);
+    let vlCupomNumb = parseFloat(vlCupom);
+    let vlTransferNumb = parseFloat(vlTransfer);
+    
+    priceMilhasReal = 70 - ((70 * vlCupomNumb) / 100);
+    valueInvest = (qtdPointNumb / 1000) * priceMilhasReal;
+
+    resultTransfer = ((qtdPointNumb * vlTransferNumb) / 100) + qtdPointNumb;
+    priceMilheiroOff = valueInvest / (resultTransfer / 1000);
+    
+    if(checkBuyPoint() == false){}
+    else 
+    {
+        if(vlTransfer == ''){
+            document.querySelector('#vl-invest').innerHTML = valueInvest.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+            document.querySelector('#vl-milheiro').innerHTML = priceMilhasReal.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+            if (valor == 1){
+                if (priceMilhasReal <= 17.50) {
+                    document.querySelector('#warning').innerHTML = 'OPORTUNIDADE NESSA CONTACAO'
+                    document.querySelector('#warning').style.color = '#21EC1D';
+                } else {
+                    document.querySelector('#warning').innerHTML = 'PREJUIZO NESSA CONTACAO'  
+                    document.querySelector('#warning').style.color = 'red';
+                }
+            } else if (valor == 2) {
+                if (priceMilhasReal <= 20) {
+                    document.querySelector('#warning').innerHTML = 'OPORTUNIDADE NESSA CONTACAO'
+                    document.querySelector('#warning').style.color = '#21EC1D';
+                } else {
+                    document.querySelector('#warning').innerHTML = 'PREJUIZO NESSA CONTACAO'  
+                    document.querySelector('#warning').style.color = 'red';
+                }
+            } else if (valor == 3 || valor == 4) {
+                if (priceMilhasReal <= 24) {
+                    document.querySelector('#warning').innerHTML = 'OPORTUNIDADE NESSA CONTACAO'
+                    document.querySelector('#warning').style.color = '#21EC1D';
+                } else {
+                    document.querySelector('#warning').innerHTML = 'PREJUIZO NESSA CONTACAO'  
+                    document.querySelector('#warning').style.color = 'red';
+                }
+            } else {
+                alert('Preecnha os Campos!')
+            }
+            //remov();    
+        } else {
+            document.querySelector('#vl-invest').innerHTML = valueInvest.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+            document.querySelector('#vl-milheiro').innerHTML = priceMilheiroOff.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+            document.querySelector('#ttl-point').innerHTML = resultTransfer;   
+            if (valor == 1){
+                if (priceMilheiroOff <= 17.50) {
+                    document.querySelector('#warning').innerHTML = 'OPORTUNIDADE NESSA CONTACAO'
+                    document.querySelector('#warning').style.color = '#21EC1D';
+                } else {
+                    document.querySelector('#warning').innerHTML = 'PREJUIZO NESSA CONTACAO'  
+                    document.querySelector('#warning').style.color = 'red';
+                }
+            } else if (valor == 2) {
+                if (priceMilheiroOff <= 20) {
+                    document.querySelector('#warning').innerHTML = 'OPORTUNIDADE NESSA CONTACAO'
+                    document.querySelector('#warning').style.color = '#21EC1D';
+                } else {
+                    document.querySelector('#warning').innerHTML = 'PREJUIZO NESSA CONTACAO'  
+                    document.querySelector('#warning').style.color = 'red';
+                }
+            } else if (valor == 3 || valor == 4) {
+                if (priceMilheiroOff <= 24) {
+                    document.querySelector('#warning').innerHTML = 'OPORTUNIDADE NESSA CONTACAO'
+                    document.querySelector('#warning').style.color = '#21EC1D';
+                } else {
+                    document.querySelector('#warning').innerHTML = 'PREJUIZO NESSA CONTACAO'  
+                    document.querySelector('#warning').style.color = 'red';
+                }
+            } else {
+                alert('Preecnha os Campos!')
+            }
+            //remov();    
+        } 
+    }       
+}
