c8540bc16a7aea318e9cb94b0e5c6ba57af43e3f
wesley teles <wesley.teles.dev@gmail.com>
2023-02-02 17:06:26 -0300

    Criando Funcao Pontos mais dinheiro e ajuste de menu

--- assets/js/transfer.js
+++ assets/js/transfer.js
@@ -1,7 +1,7 @@
 // Funcao pra checar se os campos estao vazios
 function checkField(){
     let qtdPoints = document.querySelector('#qtd-point').value;
-    let porcentPoint = document.querySelector('#vl-transfer').value;
+    let porcentPoint = document.querySelector('#qtd-percent').value;
     let msg1 = '';
 
     if (qtdPoints == ''){
@@ -17,26 +17,53 @@ function checkField(){
     return true;
 }
 
-// Funcao para limpar os campos
-function cleanField() {
-    document.querySelector('#qtd-point').value = '';
+// Funcao pra quando clica no checkbox ele mostre o input da transferencia
+let checkbox = document.querySelector('#check');
+let element = document.querySelector('.row-transfer');
+
+checkbox.addEventListener('change', ()=> {
+    element.classList.toggle("show-input-transfer");
     document.querySelector('#vl-transfer').value = '';
-}
+})
 
-function priceTransfer() {
-    let qtdPoint= document.querySelector('#qtd-point').value;
-    let vlTranfer = document.querySelector('#vl-transfer').value;
+document.querySelector('#btn-cotar').addEventListener('click', () => {
+    let qtdPoint = document.querySelector('#qtd-point').value;
+    let vlrPercent = document.querySelector('#qtd-percent').value;
+    let vlTransfer = document.querySelector('#vl-transfer').value;
+    let vlrInves = document.querySelector('#vlr-invest').value;
+
     let qtdPointNumb = parseFloat(qtdPoint);
-    let vlTransferNumb = parseFloat(vlTranfer);
+    let vlrPercentNumb = parseFloat(vlrPercent);
+    let vlrInvesNumb = parseFloat(vlrInves);
+    let vlTransferNumb = parseFloat(vlTransfer);
 
+    newResult = qtdPointNumb - vlrPercentNumb;
+    priceMilhasReal = ( vlrInvesNumb * 1000 ) / newResult ;
+    valueInvest = vlrInvesNumb;
+    
     resultTransfer = ((qtdPointNumb * vlTransferNumb) / 100) + qtdPointNumb;
+    priceMilheiroOff = valueInvest / (resultTransfer / 1000);
     
     if(checkField() == false){
     } else {
-        document.querySelector('#ttl-point').innerHTML = `${resultTransfer} Pontos`;
-        cleanField();
-    }       
-}
+        if(vlTransfer == '') {
+            document.querySelector('#vl-invest').innerHTML = valueInvest.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+            document.querySelector('#vl-milheiro').innerHTML = priceMilhasReal.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+            document.querySelector('#ttl-point').innerHTML = qtdPointNumb;
+        } else {
+            document.querySelector('#vl-invest').innerHTML = valueInvest.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+            document.querySelector('#vl-milheiro').innerHTML = priceMilheiroOff.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+            document.querySelector('#ttl-point').innerHTML = resultTransfer;   
+            } 
+        }       
+});
 
-let btnCotar = document.querySelector('#btn-cotar');
-btnCotar.addEventListener('click', priceTransfer);
+// Calculando os 20%
+let qtdPoint = document.querySelector('#qtd-point');
+let qtdPercent = document.querySelector('#qtd-percent');
+
+qtdPoint.addEventListener("change", function(){
+    qtdPercent.value = this.value * 0.2;
+});
+
+
